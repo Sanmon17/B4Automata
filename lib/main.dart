7e627fca5dda21ef838a953c20d6d7785ec19e29
Sanmon17 <120361089+Sanmon17@users.noreply.github.com>
2023-07-08 16:06:28 +0700

    Learn layout and button

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,20 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int button1 = 0, bottomButton = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.deepPurple,
@@ -20,6 +28,66 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
+        ),
+        body: Center(
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.deepPurple),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      ++button1;
+                    });
+                  },
+                  child: Text(button1.toString()),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.amber),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      ++button1;
+                    });
+                  },
+                  child: Text(button1.toString()),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Setting',
+              icon: Icon(
+                Icons.settings,
+                color: Colors.deepPurple,
+              ),
+            ),
+          ],
+          currentIndex: bottomButton,
+          onTap: (value) {
+            setState(() {
+              bottomButton = value;
+            });
+          },
         ),
       ),
     );
